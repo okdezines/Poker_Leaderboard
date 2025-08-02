c044bc5277d744fc056e318ef878178d3470efe5
ompa <nmaladina@hotmail.com>
2025-08-02 14:56:31 +1200

    added minor adjustent before making app responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,7 +20,7 @@ export default function Navbar() {
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/" className="text-white text-lg font-bold">
-          PokerLeaderboard
+          SnakePit Leaderboard
         </Link>
         <div className="space-x-4">
           <Link href="/" className="text-gray-300 hover:text-white">

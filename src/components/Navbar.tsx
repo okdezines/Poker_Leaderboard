246c7e958858966c06bc3228f096cba9132afeb5
ompa <nmaladina@hotmail.com>
2025-08-10 02:35:18 +1200

    minor image issue on local machine

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -24,7 +24,7 @@ export default function Navbar() {
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/" className="flex items-center space-x-2 text-white text-lg font-bold">
           <Image
-            src="https://placehold.co/40x40.png"
+            src="https://placehold.co/40x40"
             width={40}
             height={40}
             alt="placeholder"
